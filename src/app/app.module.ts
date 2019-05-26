a6a20f7ee9bff207d2954587ada0ae62a85a9820
Vinícius Lourenço <vinyygamerlol@gmail.com>
2019-05-26 15:15:43 -0300

    update: text of back button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,9 @@ import { AppRoutingModule } from './app-routing.module';
   entryComponents: [],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
+    IonicModule.forRoot({
+      backButtonText: 'Voltar',
+    }),
     AppRoutingModule,
   ],
   providers: [
